3e6a21328e00a34124cedfedcb771f766d4d0a61
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2018-12-19 18:13:44 +0300

    install bootstrap & added styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
-import "./App.css";
 import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="Johh Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-        <Contact
-          name="Karen Smith"
-          email="karen@gmail.com"
-          phone="333-333-3333"
-        />
+        <div className="container">
+          <Contact
+            name="Johh Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="karen@gmail.com"
+            phone="333-333-3333"
+          />
+        </div>
       </div>
     );
   }
