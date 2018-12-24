0687bd4afbab34860f66d448ffa882f1d1cbd6d9
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2018-12-24 19:09:16 +0300

    start point for Redux

--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,6 +1,6 @@
-import React from "react"; // rfc
-import PropTypes from "prop-types"; // impt
-import classnames from "classnames";
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 const TextInputGroup = ({
   label,
@@ -16,11 +16,11 @@ const TextInputGroup = ({
       <label htmlFor={name}>{label}</label>
       <input
         type={type}
-        className={classnames("form-control form-control-lg", {
-          "is-invalid": error
+        name={name}
+        className={classnames('form-control form-control-lg', {
+          'is-invalid': error
         })}
         placeholder={placeholder}
-        name={name}
         value={value}
         onChange={onChange}
       />
@@ -32,15 +32,15 @@ const TextInputGroup = ({
 TextInputGroup.propTypes = {
   label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   error: PropTypes.string
 };
 
 TextInputGroup.defaultProps = {
-  type: "text"
+  type: 'text'
 };
 
 export default TextInputGroup;
--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,4 +1,4 @@
-import React from "react"; // rafc
+import React from 'react';
 
 export default () => {
   return (
