50340553c742a04daa32262eed7bfd71bedf9cfa
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2018-12-24 18:26:16 +0300

    change BrowserRouter to HashRouter for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Contacts from "./components/Contacts/Contacts";
 import Header from "./components/Layout/Header/Header";
 import AddContact from "./components/Contacts/AddContact/AddContact";
